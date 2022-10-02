83768564576717c67df2f06e539bfa174826cc6f
unknown <nastena-alhovik@yandex.ru>
2022-10-02 22:34:04 +0300

    add Task 68 homework 9

---
+++ Task_68/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+int AkkFunct(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return AkkFunct(m - 1, 1);
+    if (m > 0 && n > 0) return AkkFunct(m - 1, AkkFunct(m, n - 1));
+    else return 0;
+}
+Console.WriteLine(AkkFunct(m,n));
