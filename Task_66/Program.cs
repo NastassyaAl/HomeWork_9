7c994459db92a1f115cbd14591c7e65ff3062755
unknown <nastena-alhovik@yandex.ru>
2022-10-02 21:48:08 +0300

    add Task 66 homework 9

---
+++ Task_66/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+int Summ(int m, int n)
+{
+    if (m > n) return 0;
+    else return n+Summ(m,n-1);
+}
+Console.WriteLine($"Сумма чисел от {m} до {n} равна " +Summ(m,n));
