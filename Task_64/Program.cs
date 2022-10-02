51957c5c0b30e870683144418dd7fd12815216ef
unknown <nastena-alhovik@yandex.ru>
2022-10-02 20:21:55 +0300

    add Task 64 homework 9

---
+++ Task_64/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int Numbers(int N)
+{
+    if (N <= 0) return 0;
+    else Console.Write($"{N} ");
+    return Numbers(N-1);
+}
+Numbers(N);
